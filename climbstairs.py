01a1348f7302756e57537f34626e79256d127bf8
Satyasripandarinadh <112747832+Satyasripandarinadh@users.noreply.github.com>
2022-09-20 14:04:25 +0530

    Create climbstairs.py

---
+++ climbstairs.py
@@ -0,0 +1,13 @@
+def climbStairs(n):
+ steps = []
+ steps.append(1)
+ steps.append(2)
+ for i in range(2, n):
+  steps.append([i - 1] + [i - 2])
+ return steps[n - 1]
+n=3
+print(climbStairs(n))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
